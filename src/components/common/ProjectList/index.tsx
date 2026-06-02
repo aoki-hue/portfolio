a864bfddcd47c4b17ef97a3e30d9705ac256d8a3
no0645web <no0645web@gmail.com>
2026-06-02 13:54:21 +0900

    priject部分のdataを修正

--- src/components/common/ProjectList/index.tsx
+++ src/components/common/ProjectList/index.tsx
@@ -1,4 +1,4 @@
-import clsx from "clsx";
+import Link from "next/link";
 import Image from "next/image";
 import styles from "./ProjectList.module.scss";
 import "keen-slider/keen-slider.min.css";
@@ -7,17 +7,19 @@ import { ProjectListProps } from "@/types/card";
 const SkillCard = ({ src, link, text }: ProjectListProps) => {
   return (
     <li className={styles.project}>
-      <div>
-        <div className={styles["project__img"]}>
-          <Image
-            src={`/images/projects/${src}.png`}
-            alt={src}
-            width={226}
-            height={148}
-          />
+      <Link href={link} target="_blank" rel="noopener noreferrer">
+        <div>
+          <div className={styles["project__img"]}>
+            <Image
+              src={`/images/projects/${src}.png`}
+              alt={src}
+              width={226}
+              height={148}
+            />
+          </div>
+          <p className={styles["project__text"]}>{text}</p>
         </div>
-        <p className={styles["project__text"]}>{text}</p>
-      </div>
+      </Link>
     </li>
   );
 };
