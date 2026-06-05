1185c009b88fc5046852d1af84eceb992a7376cc
no0645web <no0645web@gmail.com>
2026-06-05 10:54:07 +0900

    no message

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-// import { motion } from "motion/react";
 import Header from "@/components/layout/Header";
 import Hero from "@/components/sections/Hero";
 import About from "@/components/sections/About";
@@ -18,66 +17,9 @@ const Home = () => {
           <Hero />
         </div>
         <About />
-        {/* <motion.section
-          id="skills"
-          initial={{
-            opacity: 0,
-            y: 40,
-          }}
-          whileInView={{
-            opacity: 1,
-            y: 0,
-          }}
-          viewport={{
-            once: true,
-            amount: 0.3,
-          }}
-          transition={{
-            duration: 0.6,
-          }}
-        > */}
         <Skills />
-        {/* </motion.section> */}
-        {/* <motion.section
-          id="projects"
-          initial={{
-            opacity: 0,
-            y: 40,
-          }}
-          whileInView={{
-            opacity: 1,
-            y: 0,
-          }}
-          viewport={{
-            once: true,
-            amount: 0.3,
-          }}
-          transition={{
-            duration: 0.6,
-          }}
-        > */}
         <Projects />
-        {/* </motion.section> */}
-        {/* <motion.section
-          id="contact"
-          initial={{
-            opacity: 0,
-            y: 40,
-          }}
-          whileInView={{
-            opacity: 1,
-            y: 0,
-          }}
-          viewport={{
-            once: true,
-            amount: 0.3,
-          }}
-          transition={{
-            duration: 0.6,
-          }}
-        > */}
         <Contact />
-        {/* </motion.section> */}
       </main>
       <Footer />
     </>
